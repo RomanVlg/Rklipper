a90bd1f48a04c25e0508e849b92697485fc247f9
Роман <g.roman.vlg@gmail.com>
2024-03-16 21:52:54 +0300

    Добавлены шрифты

---
+++ Klipper fonts/font.sh
@@ -0,0 +1,63 @@
+#!/bin/bash 
+
+echo "Выберите шрифт"
+
+select font in font8x14_Orig.py  IBMVGA_8x14.py  Termius_1251.py  TermiusBold_1251.py  ToshibaSat_8x14.py  ToshibaTxL1_8x16.py Выход
+
+ do
+case $font in
+
+  "font8x14_Orig.py")
+     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     echo "Шрифт $font установлен"
+
+     ;;
+
+  "IBMVGA_8x14.py")
+     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     echo "Шрифт $font установлен"
+
+
+   ;;
+
+  "Termius_1251.py")
+     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     echo "Шрифт $font установлен"
+
+
+   ;;
+
+  "TermiusBold_1251.py")
+     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     echo "Шрифт $font установлен"
+
+
+   ;;
+
+  "ToshibaSat_8x14.py")
+     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     echo "Шрифт $font установлен"
+
+
+   ;;
+
+  "ToshibaTxL1_8x16.py")
+     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     echo "Шрифт $font установлен"
+
+
+   ;;
+
+
+
+*)
+sudo service klipper restart
+echo "Выход"
+
+break
+
+;;
+
+esac
+
+done
