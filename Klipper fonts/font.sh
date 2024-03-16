6d354e36d6244770771a11d6e15e5392cac75631
Роман <g.roman.vlg@gmail.com>
2024-03-16 22:17:13 +0300

    Правка скрипта

--- Klipper fonts/font.sh
+++ Klipper fonts/font.sh
@@ -8,41 +8,41 @@ select font in font8x14_Orig.py  IBMVGA_8x14.py  Termius_1251.py  TermiusBold_12
 case $font in
 
   "font8x14_Orig.py")
-     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     cp "$font" /home/roman/klipper/klippy/extras/display/font8x14.py
      echo "Шрифт $font установлен"
 
      ;;
 
   "IBMVGA_8x14.py")
-     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     cp "$font" /home/roman/klipper/klippy/extras/display/font8x14.py
      echo "Шрифт $font установлен"
 
 
    ;;
 
   "Termius_1251.py")
-     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     cp "$font" /home/roman/klipper/klippy/extras/display/font8x14.py
      echo "Шрифт $font установлен"
 
 
    ;;
 
   "TermiusBold_1251.py")
-     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     cp "$font" /home/roman/klipper/klippy/extras/display/font8x14.py
      echo "Шрифт $font установлен"
 
 
    ;;
 
   "ToshibaSat_8x14.py")
-     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     cp "$font" /home/roman/klipper/klippy/extras/display/font8x14.py
      echo "Шрифт $font установлен"
 
 
    ;;
 
   "ToshibaTxL1_8x16.py")
-     cp "/mnt/sd/Klipper fonts/$font" /home/roman/klipper/klippy/extras/display/font8x14.py
+     cp "$font" /home/roman/klipper/klippy/extras/display/font8x14.py
      echo "Шрифт $font установлен"
 
 
