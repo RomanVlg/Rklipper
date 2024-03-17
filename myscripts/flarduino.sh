77794d4f36ebf434f3285130d787710661711a63
Роман <g.roman.vlg@gmail.com>
2024-03-17 10:34:02 +0300

    Мелкие изменения

--- myscripts/flarduino.sh
+++ myscripts/flarduino.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 avrdude -p atmega328p -c arduino  -b 115200 -P /dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0 -v  -D -U flash:w:/home/roman/klipper/out/klipper.elf.hex:i
+sudo service klipper restart
